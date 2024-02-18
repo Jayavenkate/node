964ebbd7f8549b92a7e6c51cc2767c10a87fd734
Jayavenkate <jayajaikithuja@gmail.com>
2024-02-18 23:32:03 +0300

    cors

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.options("*", cors());
 const io = socketio(server, {
   cors: { origin: "http://localhost:5173/", methods: ["GET", "POST"] },
 });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 //socket
 
 io.on("connect", (socket) => {
